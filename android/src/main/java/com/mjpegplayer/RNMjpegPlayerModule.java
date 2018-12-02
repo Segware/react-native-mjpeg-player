e9d30a018b82b7148b338b1a87b9696d3b9fc9a9
Mathias5R <mathiassilva4@gmail.com>
2018-12-02 21:19:49 -0200

    Change package name

--- android/src/main/java/com/mjpegplayer/RNMjpegPlayerModule.java
+++ android/src/main/java/com/mjpegplayer/RNMjpegPlayerModule.java
@@ -1,10 +1,8 @@
 
-package com.reactlibrary;
+package com.mjpegplayer;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
-import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
 
 public class RNMjpegPlayerModule extends ReactContextBaseJavaModule {
 
--- android/src/main/java/com/mjpegplayer/RNMjpegPlayerPackage.java
+++ android/src/main/java/com/mjpegplayer/RNMjpegPlayerPackage.java
@@ -1,5 +1,5 @@
 
-package com.reactlibrary;
+package com.mjpegplayer;
 
 import java.util.Arrays;
 import java.util.Collections;
