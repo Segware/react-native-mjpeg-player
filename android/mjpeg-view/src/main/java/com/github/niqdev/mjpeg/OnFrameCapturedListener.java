1b90c29a7eef962652c7ea99b5468a6f93ad5e83
Mathias5R <mathiassilva4@gmail.com>
2018-12-08 15:45:42 -0200

    Removing mjpeg lib

--- android/mjpeg-view/src/main/java/com/github/niqdev/mjpeg/OnFrameCapturedListener.java
+++
@@ -1,7 +0,0 @@
-package com.github.niqdev.mjpeg;
-
-import android.graphics.Bitmap;
-
-public interface OnFrameCapturedListener {
-    void onFrameCaptured(Bitmap bitmap);
-}
--- android/src/main/java/com/mjpegplayer/RNMjpegPlayerManager.java
+++ android/src/main/java/com/mjpegplayer/RNMjpegPlayerManager.java
@@ -1,8 +1,10 @@
 package com.mjpegplayer;
 
+import android.graphics.Color;
 import android.net.Uri;
 import android.support.annotation.Nullable;
 import android.util.Log;
+import android.view.View;
 
 import com.facebook.infer.annotation.Assertions;
 import com.facebook.react.bridge.ReadableArray;
@@ -16,11 +18,10 @@ import com.facebook.react.uimanager.annotations.ReactProp;
 import java.util.Map;
 
 
-public class RNMjpegPlayerManager extends SimpleViewManager<MjpegPlayerView> {
+public class RNMjpegPlayerManager extends SimpleViewManager<View> {
     public static final String REACT_CLASS = "MjpegPlayer";
     private ThemedReactContext mContext;
-    private MjpegPlayerView mjpegPlayerView;
-    private static final int COMMAND_STOP_VIDEO = 1;
+    private View view;
 
     @Override
     public String getName() {
@@ -28,66 +29,16 @@ public class RNMjpegPlayerManager extends SimpleViewManager<MjpegPlayerView> {
     }
 
     @Override
-    public MjpegPlayerView createViewInstance(ThemedReactContext context){
+    public View createViewInstance(ThemedReactContext context){
         mContext = context;
-        mjpegPlayerView = new MjpegPlayerView(context);
-        return mjpegPlayerView;
+        view = new View(context);
+        view.setBackgroundColor(Color.BLUE);
+        return view;
     }
 
-    @ReactProp(name = "settings")
-    public void setVideoPath(MjpegPlayerView mjpegPlayerView, ReadableMap settings) {
-        String url = settings.getString("url");
-        int width = settings.getInt("width");
-        int height = settings.getInt("height");
-        Uri uri = Uri.parse(url);
-        mjpegPlayerView.setFrameWidth(width);
-        mjpegPlayerView.setFrameHeight(height);
-        mjpegPlayerView.setVideoURI(uri);
-        mjpegPlayerView.start();
+    @ReactProp(name = "exampleProp")
+    public void setExampleProp(View view, String prop) {
+        // TODO
     }
 
-    @Override
-    public Map<String,Integer> getCommandsMap() {
-        Log.d("React"," View manager getCommandsMap:");
-        return MapBuilder.of(
-                "stopVideo",
-                COMMAND_STOP_VIDEO);
-    }
-
-    @Override
-    public void receiveCommand(
-            MjpegPlayerView mjpegPlayerView,
-            int commandType,
-            @Nullable ReadableArray args) {
-        Assertions.assertNotNull(mjpegPlayerView);
-        Assertions.assertNotNull(args);
-        switch (commandType) {
-            case COMMAND_STOP_VIDEO: {
-                mjpegPlayerView.stop();
-                return;
-            }
-
-            default:
-                throw new IllegalArgumentException(String.format(
-                        "Unsupported command %d received by %s.",
-                        commandType,
-                        getClass().getSimpleName()));
-        }
-    }
-
-    @Override
-    public Map getExportedCustomBubblingEventTypeConstants() {
-        return MapBuilder.builder()
-                .put(
-                        "datetimeChange",
-                        MapBuilder.of(
-                                "phasedRegistrationNames",
-                                MapBuilder.of("bubbled", "onDateTimeChange")))
-                .put(
-                        "mjpegError",
-                        MapBuilder.of(
-                                "phasedRegistrationNames",
-                                MapBuilder.of("bubbled", "onMjpegError")))
-                .build();
-    }
 }
